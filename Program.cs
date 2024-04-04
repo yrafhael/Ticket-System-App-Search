799b6123c6b7fbecff7b4fd9412f411f4fcee9ef
Yose Rafhael <yrafhael@my.wctc.edu>
2024-04-04 10:04:03 -0500

    [1.06] Create prompt for user - user would be able to read data from files - user would be able to create file from data - user would be able to search through ticket

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        
+            Console.WriteLine("1) Read data from Tickets file.");
+            Console.WriteLine("2) Read data from Enhancements file.");
+            Console.WriteLine("3) Read data from Tasks file.");
+            Console.WriteLine("4) Create file from data.");
+            Console.WriteLine("5) Search through Tickets file.");
+            Console.WriteLine("Enter any other key to exit.");
+    }
+}
